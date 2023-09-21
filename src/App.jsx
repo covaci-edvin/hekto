db35a420193a97b949a14492cea3e400c017962f
ecovaci <ecovaci@griddynamics.com>
2023-09-21 19:48:33 +0300

    font-setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import { useState } from "react";
 
 function App() {
-
-  return <></>;
+  return (
+    <>
+      <h1 className="heading-1">Heading 1</h1>
+      <h2 className="heading-2">Heading 2</h2>
+      <h3 className="heading-3">Heading 3</h3>
+      <h4 className="heading-4">Heading 4</h4>
+      <p className="subtitle-1">Subtitle 1</p>
+      <p className="subtitle-2">Subtitle 2</p>
+      <p className="subtitle-3">Subtitle 3</p>
+      <p className="subtitle-4">Subtitle 4</p>
+      <p className="body-text-lg">body text large</p>
+      <p className="body-text-md">body text medium</p>
+      <p className="body-text-md-bold">body text medium bold</p>
+      <p className="body-text-sm">body text medium small</p>
+    </>
+  );
 }
 
 export default App;
